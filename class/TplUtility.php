941e0f2b2789d3ef6c398e6c63b9c0aecb2b80c2
gerardnico <gerardnico@gmail.com>
2022-01-07 20:59:57 +0100

    Added search ajax

--- class/TplUtility.php
+++ class/TplUtility.php
@@ -774,28 +774,7 @@ EOF;
 
     }
 
-    function renderSearchForm($ajax = true, $autocomplete = true)
-    {
-        global $lang;
-        global $ACT;
-        global $QUERY;
 
-        // don't print the search form if search action has been disabled
-        if (!actionOK('search')) return false;
-
-        print '<form id="navBarSearch" action="' . wl() . '" accept-charset="utf-8" class="search form-inline my-lg-0" id="dw__search" method="get" role="search">';
-        print '<input type="hidden" name="do" value="search" />';
-        print '<label class="sr-only" for="search">Search Term</label>';
-        print '<input type="text" ';
-        if ($ACT == 'search') print 'value="' . htmlspecialchars($QUERY) . '" ';
-        print 'placeholder="' . $lang['btn_search'] . '..." ';
-        if (!$autocomplete) print 'autocomplete="off" ';
-        print 'id="qsearch__in" accesskey="f" name="id" class="edit form-control" title="[F]" />';
-//    print '<button type="submit" title="'.$lang['btn_search'].'">'.$lang['btn_search'].'</button>';
-        if ($ajax) print '<div id="qsearch__out" class="ajax_qsearch JSpopup"></div>';
-        print '</form>';
-        return true;
-    }
 
     /**
      * This is a fork of tpl_actionlink where I have added the class parameters
